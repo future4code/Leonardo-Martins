594d3a1400fcd2c021a3388792ce77144f4d2d85
Leonardo Fernandes Martins <fmartins.leonardo@gmail.com>
2021-10-11 14:45:42 -0300

    esqueleto

---
+++ react/labenusers/src/Components/DetalheUsuario.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const DivButton = styled.div`
+  button {
+    text-align: center;
+    width: 150px;
+    height: 36px;
+    margin: 10px;
+    border-radius: 8px;
+    background-color: black;
+    color: lightsalmon;
+    :hover {
+      background-color: lightsalmon;
+      color: black;
+    }
+  }
+`;
+
+export default class DetalheUsuario extends React.Component {
+  render() {
+    return (
+      <div>
+        <DivButton>
+          <button onClick={this.props.irParaCadastro}>Cadastro</button>
+          <button onClick={this.props.irParaLista}>Lista de Usuários</button>
+        </DivButton>
+        <h2>Detalhe Dos Usuários</h2>
+      </div>
+    );
+  }
+}
