c119546c14833eaf4f25cfa947cec4c64ff2b24e
Leonardo Fernandes Martins <fmartins.leonardo@gmail.com>
2021-10-31 19:13:10 -0300

    atualizacao feed

--- revisao/labeddit/src/pages/FeedPage/FeedPage.js
+++ revisao/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router";
-import {
-  goToLogin,
-  goToPost,
-  goToRegistration,
-} from "../../routes/coordinator";
+import { goToPost } from "../../routes/coordinator";
 import useProtectedPage from "../../hooks/useProtectedPage";
+import useRequestData from "../../hooks/useRequestData";
+import { BASE_URL } from "../../constants/urls";
 
 const baseUrl = "https://labeddit.herokuapp.com";
 
 const FeedPage = () => {
   useProtectedPage();
   const history = useHistory();
+
+  const feed = useRequestData([], `${BASE_URL}/posts`);
+  console.log(feed);
 
   return (
     <div>
